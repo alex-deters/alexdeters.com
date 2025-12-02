4b46d02f93a8ec8b19a6777966e86a3b1d43c5c3
Alex Deters <adeters@g.harvard.edu>
2025-12-02 15:04:59 -0500

    cv

--- src/routes/__layout.svelte
+++ src/routes/__layout.svelte
@@ -30,7 +30,7 @@
       <a href="/education" class="block mt-4 md:inline-block md:mt-0 hover:text-flamingo mr-4  {url == '/education' ? 'text-red' : ''}" on:click="{() => menuOpened=!menuOpened}">
         Education
       </a>
-      <a target="_blank" href="https://drive.google.com/file/d/1YAYpniNUjb1M_g5XKG0EWxWRJ55zYoQC/view?usp=sharing" class="block mt-4 md:inline-block md:mt-0 hover:text-red mr-4 md:mr-0" on:click="{() => menuOpened=!menuOpened}">
+      <a target="_blank" href="https://drive.google.com/file/d/1JlF0eGr2xKA1gSrJGHvuiuVbaZulgmf2/view?usp=drive_link" class="block mt-4 md:inline-block md:mt-0 hover:text-red mr-4 md:mr-0" on:click="{() => menuOpened=!menuOpened}">
         CV
       </a>
     </div>
